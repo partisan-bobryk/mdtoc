c0b4fa6970b318a6e59bda82d73493e6a9329d91
15-pieces-of-flair <maks@revent.studio>
2022-09-12 07:09:55 -0500

    make sure rust installation persists

--- scripts/install_rust.sh
+++ scripts/install_rust.sh
@@ -1,27 +1,27 @@
 #!/usr/bin/env bash
-
 # Exit script if you try to use an uninitialized variable.
 set -o nounset
-
 # Exit script if a statement returns a non-true return value.
 set -o errexit
-
 # Use the error status of the first failure, rather than that of the last item in a pipeline.
 set -o pipefail
-
 set -o xtrace
 
-RUST_VERSION=stable
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -o rustup.sh
 chmod +x rustup.sh
 ./rustup.sh -y
 
 source "$HOME"/.cargo/env
 
+printenv
+
+
 if [ "$RUST_VERSION" != "stable" ]; then
 	rustup toolchain install "$RUST_VERSION"
 	rustup default "$RUST_VERSION"
 fi
+echo "Where is rust installed?"
+which rustup
 
 # Install toolchains
 rustup target install aarch64-apple-darwin
