db08b4631726d9f8bd11c7468fae854e969d79c5
15-pieces-of-flair <maks@revent.studio>
2022-09-11 22:23:23 -0500

    Adds rust installation

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+RUST_VERSION=stable
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -o rustup.sh
+chmod +x rustup.sh
+./rustup.sh -y
+
+source "$HOME"/.cargo/env
+
+if [ "$RUST_VERSION" != "stable" ]; then
+	rustup toolchain install "$RUST_VERSION"
+	rustup default "$RUST_VERSION"
+fi
 
 BUILD_PATH="bin"
 MACOS_ARM=aarch64-apple-darwin
