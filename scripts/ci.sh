d4391df8feef5e58725b05ab6a7de78c10661639
15-pieces-of-flair <maks@revent.studio>
2022-09-12 06:56:08 -0500

    Enable matrix mode

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -14,10 +14,8 @@ set -o xtrace
 source "$HOME"/.cargo/env
 
 BUILD_PATH="bin"
-MACOS_ARM=aarch64-apple-darwin
-MACOS_INTEL=x86_64-apple-darwin
+BUILD_TARGET=$1
 
-cargo build --release --target $MACOS_ARM --target-dir "$BUILD_PATH/$MACOS_ARM"
-cargo build --release --target $MACOS_INTEL --target-dir "$BUILD_PATH/$MACOS_INTEL"
+cargo build --release --target $BUILD_TARGET --target-dir "$BUILD_PATH/$BUILD_TARGET"
 
 ls -al "$BUILD_PATH"
